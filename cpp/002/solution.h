d5af7a9f84930ce04a863ac4d963dd2f8f4de0ef
Stephan Bischoff <stephan.bischoff.hg@googlemail.com>
2019-11-05 15:25:28 +0100

    Solve day 2 problem

---
+++ cpp/002/solution.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <numeric>
+#include <vector>
+
+
+template <typename T>
+std::vector<T> with_divide(const std::vector<T>& input) {
+    T product = std::accumulate(
+            input.begin(), input.end(), T{1}, std::multiplies<T>());
+    std::vector<T> result;
+
+    for (auto&& x : input) {
+        result.emplace_back(product / x);
+    }
+
+    return result;
+}
