a371c9d1a5e38b655dc0db272ff27480cd4acc5a
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-07 17:01:40 +0100

    Scafold problem 4

---
+++ cpp/004/solution.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+
+int find_lowest_missing(std::vector<int>& input) {
+    return input[0];
+}
---
+++ cpp/004/tests.cc
@@ -0,0 +1,18 @@
+#include "solution.h"
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+TEST(d004, readme_01) {
+    std::vector<int> input{3, 4, -1, 1};
+
+    ASSERT_EQ(find_lowest_missing(input), 2);
+}
+
+TEST(d004, readme_02) {
+    std::vector<int> input{1, 2, 0};
+
+    ASSERT_EQ(find_lowest_missing(input), 3);
+}
+
