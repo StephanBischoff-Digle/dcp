aec243eb728908dbdd507c141f48030a847de0e4
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-07 18:28:55 +0100

    Solve Problem 4

--- cpp/004/solution.h
+++ cpp/004/solution.h
@@ -2,6 +2,38 @@
 
 #include <vector>
 
+#include "dbg.h"
+
 int find_lowest_missing(std::vector<int>& input) {
-    return input[0];
+
+    dbg(input);
+
+    for (std::size_t i = 0; i < input.size();) {
+        auto current = input[i];
+
+        if (current > 0 && static_cast<std::size_t>(current) <= input.size()) {
+            if (static_cast<std::size_t>(current - 1) == i) {
+                i++;
+                continue;
+            }
+
+            auto other = input[static_cast<std::size_t>(current - 1)];
+            input[static_cast<std::size_t>(current - 1)] = current;
+            input[i] = other;
+        } else {
+            i++;
+        }
+        dbg(input);
+    }
+
+
+    dbg(input);
+
+    for (std::size_t i = 0; i <= input.size(); i++) {
+        if (i + 1 != static_cast<std::size_t>(input[i])) {
+            return static_cast<int>(i + 1);
+        }
+    }
+
+    return static_cast<int>(input.size());
 }
