ddd69b6b240feae246efef0aad5b80f6f78c74cf
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-16 15:59:27 +0100

    Add solution for problem 5

---
+++ cpp/005/solution.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <functional>
+
+
+auto cons(int a, int b) {
+    return [a, b](std::function<int(int, int)> f) {return f(a, b);};
+}
+
+int car(std::function<int(std::function<int(int, int)>)> p) {
+    return p([](int a, int) {return a;});
+}
+
+int cdr(std::function<int(std::function<int(int, int)>)> p) {
+    return p([](int, int b) {return b;});
+}
