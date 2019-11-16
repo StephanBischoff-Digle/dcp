dd32cccdcf4ac0087e22b88e2eaf1bb8d28cc164
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-16 16:00:50 +0100

    Fix formating

--- cpp/005/solution.h
+++ cpp/005/solution.h
@@ -2,15 +2,14 @@
 
 #include <functional>
 
-
 auto cons(int a, int b) {
-    return [a, b](std::function<int(int, int)> f) {return f(a, b);};
+    return [a, b](std::function<int(int, int)> f) { return f(a, b); };
 }
 
 int car(std::function<int(std::function<int(int, int)>)> p) {
-    return p([](int a, int) {return a;});
+    return p([](int a, int) { return a; });
 }
 
 int cdr(std::function<int(std::function<int(int, int)>)> p) {
-    return p([](int, int b) {return b;});
+    return p([](int, int b) { return b; });
 }
