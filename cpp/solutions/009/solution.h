84e5e9f26d6987abbd9b9bf6a2297b17aedaf76c
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-23 20:02:53 +0100

    Add recursive solution for problem 9

--- cpp/solutions/009/solution.h
+++ cpp/solutions/009/solution.h
@@ -1,3 +1,20 @@
 #pragma once
 
-void init() {}
+#include <vector>
+#include <algorithm>
+
+
+template <typename T>
+T recursive_highest_non_adjacent(
+        const typename std::vector<T>::const_iterator& start,
+        const typename std::vector<T>::const_iterator& end) {
+
+    if (start >= end) {
+        return 0;
+    }
+
+    return std::max(
+            recursive_highest_non_adjacent<T>(start + 1, end),
+            *start + recursive_highest_non_adjacent<T>(start + 2, end));
+
+}
--- cpp/solutions/009/tests.cc
+++ cpp/solutions/009/tests.cc
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
 #include "solution.h"
 
-TEST(d009, init) {
+#include <vector>
 
-    ASSERT_EQ(false, true);
+TEST(d009, recursive_readme_1) {
+    std::vector<int> v = {2, 4, 6, 2, 5};
+
+    ASSERT_EQ(recursive_highest_non_adjacent<int>(v.begin(), v.end()), 13);
+}
+
+TEST(d009, recursive_readme_2) {
+    std::vector<int> v = {5, 1, 1, 5};
+
+    ASSERT_EQ(recursive_highest_non_adjacent<int>(v.begin(), v.end()), 10);
 }
 
 
