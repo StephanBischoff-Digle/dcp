be6bdd133bd944764023f2fc9c638e75b845b5ca
Stephan Bischoff <stephan.bischoff@digle.net>
2019-11-23 20:28:33 +0100

    Add dynmaic solution for problem 9

--- cpp/solutions/009/solution.h
+++ cpp/solutions/009/solution.h
@@ -18,3 +18,26 @@ T recursive_highest_non_adjacent(
             *start + recursive_highest_non_adjacent<T>(start + 2, end));
 
 }
+
+
+template <typename T>
+T dynamic_highest_non_adjacent(const std::vector<T>& list) {
+    if (list.size() <= 2) {
+        T highest = 0;
+        for (const T& i : list) {
+            highest = std::max(highest, i);
+        }
+        return highest;
+    }
+
+    std::vector<T> cache(list.size(), T{});
+    cache[0] = std::max(0, list[0]);
+    cache[1] = std::max(cache[0], list[1]);
+
+    for (std::size_t i = 2; i < list.size(); i++) {
+        T tmp = list[i];
+        cache[i] = std::max(tmp + cache[i - 2], cache[i - 1]);
+    }
+    
+    return cache.back();
+}
--- cpp/solutions/009/tests.cc
+++ cpp/solutions/009/tests.cc
@@ -15,4 +15,15 @@ TEST(d009, recursive_readme_2) {
     ASSERT_EQ(recursive_highest_non_adjacent<int>(v.begin(), v.end()), 10);
 }
 
+TEST(d009, dynamic_readme_1) {
+    std::vector<int> v = {2, 4, 6, 2, 5};
+
+    ASSERT_EQ(dynamic_highest_non_adjacent(v), 13);
+}
+
+TEST(d009, dynamic_readme_2) {
+    std::vector<int> v = {5, 1, 1, 5};
+
+    ASSERT_EQ(dynamic_highest_non_adjacent<int>(v), 10);
+}
 
